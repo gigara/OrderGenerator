ec136f9d278798f2d8edd488c548d7b3bfefbf05
Gigara <info@gigara.info>
2020-07-19 19:45:54 +0530

    Completed all the functions

--- src/main/java/com/ordergeneration/OrderGenerator.java
+++ src/main/java/com/ordergeneration/OrderGenerator.java
@@ -3,6 +3,9 @@ package com.ordergeneration;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
@@ -10,8 +13,10 @@ import java.util.List;
 import java.util.Random;
 
 public class OrderGenerator {
+    // simulator URL
     private final WebClient webClient = WebClient.create("https://gigara.info");
-    private ArrayList<Order> orders = new ArrayList<>();
+    private final ArrayList<Order> orders = new ArrayList<>();
+    private String callBackURL = null;
 
     int[] items = {2, 3, 4, 5};
     int[] itemProbs = {50, 25, 13, 6};
@@ -23,31 +28,41 @@ public class OrderGenerator {
 
     // generate random order
     public void generate() throws JsonProcessingException {
-        // get data from simulator
-        JsonNode node = this.webClient.get().uri("/userFiles/items.json")
-                .retrieve()
-                .bodyToMono(JsonNode.class)
-                .block();
+        // 20% chance
+        boolean true20 = new Random().nextInt(5) == 0;
+        if (true20) {
+            // get data from simulator
+            JsonNode node = this.webClient.get().uri("/userFiles/items.json")
+                    .retrieve()
+                    .bodyToMono(JsonNode.class)
+                    .block();
 
-        JsonNode itemDetails = node != null ? node.get("items") : null;
-        if (itemDetails != null) {
-            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode itemDetails = node != null ? node.get("items") : null;
+            if (itemDetails != null) {
+                ObjectMapper objectMapper = new ObjectMapper();
 
-            // generate random item count
-            ArrayList<OrderItem> orderItems = new ArrayList<>();
-            int itemCount = getRandom(items, itemProbs);
-            for (int i = 1; i <= itemCount; i++) {
-                if (itemDetails.get(i) != null) {
-                    Item item = objectMapper.readValue(itemDetails.get(i).toString(), Item.class);
+                // generate random item count
+                ArrayList<OrderItem> orderItems = new ArrayList<>();
+                int itemCount = getRandom(items, itemProbs);
+                for (int i = 1; i <= itemCount; i++) {
+                    if (itemDetails.get(i) != null) {
+                        Item item = objectMapper.readValue(itemDetails.get(i).toString(), Item.class);
 
-                    // random quantity
-                    int quantity = getRandom(quantities, quantitiesProbs);
-                    orderItems.add(new OrderItem(item, quantity));
+                        // random quantity
+                        int quantity = getRandom(quantities, quantitiesProbs);
+                        orderItems.add(new OrderItem(item, quantity));
+                    }
+                }
+
+                // save generated order
+                Order order = new Order(orders.size(), OrderStatus.NEW, orderItems);
+                orders.add(order);
+
+                // notify the server
+                if (callBackURL != null) {
+                    notifyServer(order);
                 }
             }
-
-            // save generated order
-            orders.add(new Order(orders.size(), OrderStatus.NEW, orderItems));
         }
     }
 
@@ -60,6 +75,47 @@ public class OrderGenerator {
             if (order.getOrderID() == orderID) return order;
         }
         return null;
+    }
+
+    public boolean updateStatus(int orderID, JsonNode body) {
+        JsonNode status = body.get("status");
+        // check request body
+        if (status == null ||
+                (!status.asText().equals(OrderStatus.NEW.toString())
+                        && !status.asText().equals(OrderStatus.PROCESSING.toString())
+                        && !status.asText().equals(OrderStatus.COMPLETED.toString()))) return false;
+
+        for (Order order : orders) {
+            if (order.getOrderID() == orderID) {
+                order.setOrderStatus(OrderStatus.valueOf(status.asText()));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean register(JsonNode body) {
+        JsonNode ip = body.get("address");
+        if (ip != null) {
+            callBackURL = ip.asText();
+            return true;
+        }
+        return false;
+    }
+
+    private void notifyServer(Order order) {
+        // create an instance of RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        // send POST request
+        ResponseEntity<Void> response = restTemplate.postForEntity(callBackURL, order, Void.class);
+
+        // check response
+        if (response.getStatusCode() == HttpStatus.OK) {
+            System.out.println("Request Successful");
+        } else {
+            System.out.println("Request Failed");
+        }
     }
 
     private int getRandom(int[] items, int[] probs) {
--- src/main/java/com/ordergeneration/WebController.java
+++ src/main/java/com/ordergeneration/WebController.java
@@ -1,7 +1,7 @@
 package com.ordergeneration;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,38 +13,62 @@ import java.util.List;
 public class WebController {
     OrderGenerator generator = new OrderGenerator();
 
+    /**
+     * Generate order
+     *
+     * @return 200
+     * @throws JsonProcessingException request data error
+     */
     @RequestMapping(value = "/generateOrder", method = RequestMethod.GET)
     public ResponseEntity<?> generateOrder() throws JsonProcessingException {
         generator.generate();
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /**
+     * return order details
+     *
+     * @return order details
+     */
     @RequestMapping(value = "/orders", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     List<Order> getOrders() {
         return generator.getOrders();
     }
 
+    /**
+     * return order details by order ID
+     *
+     * @param id order ID
+     * @return order details
+     */
     @RequestMapping(value = "/orders/{id}", method = RequestMethod.GET)
     public @ResponseBody
     Order getOrderDetails(@PathVariable int id) {
         return generator.getOrder(id);
     }
 
+    /**
+     * update order details
+     *
+     * @param id   order id
+     * @param body order status
+     */
     @RequestMapping(value = "orders/{id}/updateStatus", method = RequestMethod.PUT)
-    public void setOrderStatus(@PathVariable String id, @RequestBody String description) {
-
+    public ResponseEntity<?> setOrderStatus(@PathVariable int id, @RequestBody JsonNode body) {
+        return generator.updateStatus(id, body) ?
+                new ResponseEntity<>(HttpStatus.ACCEPTED) : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @RequestMapping(value = "/registry", method = RequestMethod.POST)
-    public void registration(@RequestBody String description) {
-        System.out.println("Service is " + description);
-//        itemsDetails.RegisterService(description);
-    }
-
-    @RequestMapping(value = "/step", method = RequestMethod.PUT)
-    public void Step(@RequestBody String step) {
-        System.out.println(step);
-//        itemsDetails.ClockSync();
+    /**
+     * Register for new order listening
+     *
+     * @param body listening server address
+     * @return 202 if success
+     */
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<?> registration(@RequestBody JsonNode body) {
+        return generator.register(body) ?
+                new ResponseEntity<>(HttpStatus.ACCEPTED) : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
